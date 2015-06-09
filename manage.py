4f7f72f63ee0f70f8b17c858b8dce7ab290f2035
ceclinux <src655@gmail.com>
2015-06-10 03:03:08 +0800

    change host

--- manage.py
+++ manage.py
@@ -14,7 +14,7 @@ def make_shell_context():
 manager.add_command('shell', Shell(make_context = make_shell_context))
 manager.add_command('db', MigrateCommand)
 port=int(os.environ.get('PORT',  5000))
-manager.add_command('runserver', Server(port=port))
+manager.add_command('runserver', Server(port=port, host="0.0.0.0"))
 
 @manager.command
 def deploy():
